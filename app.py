e0862e9c29fcfc60e1569e5cd6a522864eb65aea
Satya <srisatyalokesh2.12@gmail.com>
2020-02-19 14:56:56 +0530

    Create app.py

--- app.py
+++ app.py
@@ -1,13 +1,6 @@
 import dash
-import dash_core_components as dcc
 import dash_html_components as html
-import pandas as pd
-import numpy as np
 
-from dash.dependencies import Input, Output
-from plotly import graph_objs as go
-from plotly.graph_objs import *
-from datetime import datetime as dt
 
 
 app = dash.Dash(
