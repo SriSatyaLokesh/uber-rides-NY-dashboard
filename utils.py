8ba9a0e209bde61d95527d65d006b9f90ef52c2a
Satya <srisatyalokesh2.12@gmail.com>
2020-02-22 11:29:03 +0530

    Layout of dashboard

---
+++ utils.py
@@ -0,0 +1,14 @@
+def get_locations():
+    # Dictionary of important locations in New York
+    list_of_locations = {
+        "Madison Square Garden": {"lat": 40.7505, "lon": -73.9934},
+        "Yankee Stadium": {"lat": 40.8296, "lon": -73.9262},
+        "Empire State Building": {"lat": 40.7484, "lon": -73.9857},
+        "New York Stock Exchange": {"lat": 40.7069, "lon": -74.0113},
+        "JFK Airport": {"lat": 40.644987, "lon": -73.785607},
+        "Grand Central Station": {"lat": 40.7527, "lon": -73.9772},
+        "Times Square": {"lat": 40.7589, "lon": -73.9851},
+        "Columbia University": {"lat": 40.8075, "lon": -73.9626},
+        "United Nations HQ": {"lat": 40.7489, "lon": -73.9680},
+    }
+    return list_of_locations
