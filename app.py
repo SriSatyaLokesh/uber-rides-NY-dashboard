4450265dc50b33c0aca3135a59ef64352cfb6d86
Satya <srisatyalokesh2.12@gmail.com>
2020-03-15 17:37:27 +0530

    added histogram functionality

--- app.py
+++ app.py
@@ -143,6 +143,29 @@ app.layout = html.Div(
 
 
 
+# Selected Data in the Histogram updates the Values in the DatePicker
+@app.callback(
+    Output("bar-selector", "value"),
+    [Input("histogram", "selectedData"), Input("histogram", "clickData")],
+)
+def update_bar_selector(value, clickData):
+    holder = []
+    if clickData:
+        holder.append(str(int(clickData["points"][0]["x"])))
+    if value:
+        for x in value["points"]:
+            holder.append(str(int(x["x"])))
+    return list(set(holder))
+
+
+# Clear Selected Data if Click Data is used
+@app.callback(Output("histogram", "selectedData"), [Input("histogram", "clickData")])
+def update_selected_data(clickData):
+    if clickData:
+        return {"points": []}
+
+
+
 
 if __name__ == "__main__":
     app.run_server(debug=True)
