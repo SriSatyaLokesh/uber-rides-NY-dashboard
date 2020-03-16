9006d76d03c516d19aa41bad38266d574b97df27
Satya <srisatyalokesh2.12@gmail.com>
2020-03-16 16:47:55 +0530

    Create utils.py

---
+++ test/utils.py
@@ -0,0 +1,60 @@
+def getLocations():
+    # Dictionary of important locations in New York
+    listOfLocations = {
+        "Madison Square Garden": {"lat": 40.7505, "lon": -73.9934},
+        "Yankee Stadium": {"lat": 40.8296, "lon": -73.9262},
+        "Empire State Building": {"lat": 40.7484, "lon": -73.9857},
+        "New York Stock Exchange": {"lat": 40.7069, "lon": -74.0113},
+        "JFK Airport": {"lat": 40.644987, "lon": -73.785607},
+        "Grand Central Station": {"lat": 40.7527, "lon": -73.9772},
+        "Times Square": {"lat": 40.7589, "lon": -73.9851},
+        "Columbia University": {"lat": 40.8075, "lon": -73.9626},
+        "United Nations HQ": {"lat": 40.7489, "lon": -73.9680},
+    }
+    return listOfLocations
+
+
+
+def getSelection(month, day, selection):
+    global totalList
+    xVal = []
+    yVal = []
+    xSelected = []
+    colorVal = [
+        "#F4EC15",
+        "#DAF017",
+        "#BBEC19",
+        "#9DE81B",
+        "#80E41D",
+        "#66E01F",
+        "#4CDC20",
+        "#34D822",
+        "#24D249",
+        "#25D042",
+        "#26CC58",
+        "#28C86D",
+        "#29C481",
+        "#2AC093",
+        "#2BBCA4",
+        "#2BB5B8",
+        "#2C99B4",
+        "#2D7EB0",
+        "#2D65AC",
+        "#2E4EA4",
+        "#2E38A4",
+        "#3B2FA0",
+        "#4E2F9C",
+        "#603099",
+    ]
+
+    # Put selected times into a list of numbers xSelected
+    xSelected.extend([int(x) for x in selection])
+
+    for i in range(24):
+        # If bar is selected then color it white
+        if i in xSelected and len(xSelected) < 24:
+            colorVal[i] = "#FFFFFF"
+        xVal.append(i)
+        # Get the number of rides at a particular time
+        yVal.append(len(totalList[month][day][totalList[month][day].index.hour == i]))
+    return [np.array(xVal), np.array(yVal), np.array(colorVal)]
