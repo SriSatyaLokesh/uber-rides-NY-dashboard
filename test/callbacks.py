7bd249c1b79da8d3a273a403bba0fb2e42f8edda
Satya <srisatyalokesh2.12@gmail.com>
2020-03-16 16:50:26 +0530

    Update callbacks.py

--- test/callbacks.py
+++ test/callbacks.py
@@ -28,3 +28,49 @@ def update_total_rides(datePicked):
     return "Total Number of rides: {:,d}".format(
         len(totalList[date_picked.month - 4][date_picked.day - 1])
     )
+
+# Update the total number of rides in selected times
+@app.callback(
+    [Output("total-rides-selection", "children"), Output("date-value", "children")],
+    [Input("date-picker", "date"), Input("bar-selector", "value")],
+)
+def update_total_rides_selection(datePicked, selection):
+    firstOutput = ""
+
+    if selection is not None or len(selection) is not 0:
+        date_picked = dt.strptime(datePicked, "%Y-%m-%d")
+        totalInSelection = 0
+        for x in selection:
+            totalInSelection += len(
+                totalList[date_picked.month - 4][date_picked.day - 1][
+                    totalList[date_picked.month - 4][date_picked.day - 1].index.hour
+                    == int(x)
+                ]
+            )
+        firstOutput = "Total rides in selection: {:,d}".format(totalInSelection)
+
+    if (
+        datePicked is None
+        or selection is None
+        or len(selection) is 24
+        or len(selection) is 0
+    ):
+        return firstOutput, (datePicked, " - showing hour(s): All")
+
+    holder = sorted([int(x) for x in selection])
+
+    if holder == list(range(min(holder), max(holder) + 1)):
+        return (
+            firstOutput,
+            (
+                datePicked,
+                " - showing hour(s): ",
+                holder[0],
+                "-",
+                holder[len(holder) - 1],
+            ),
+        )
+
+    holder_to_string = ", ".join(str(x) for x in holder)
+    return firstOutput, (datePicked, " - showing hour(s): ", holder_to_string)
+
