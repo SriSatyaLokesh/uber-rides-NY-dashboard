88d90f1997f8e0849eb582a4935aa737d2550b8c
Satya <srisatyalokesh2.12@gmail.com>
2020-03-16 16:49:07 +0530

    Create callbacks.py

---
+++ test/callbacks.py
@@ -0,0 +1,15 @@
+
+
+# Selected Data in the Histogram updates the Values in the DatePicker
+@app.callback(
+    Output("bar-selector", "value"),
+    [Input("histogram", "selectedData"), Input("histogram", "clickData")],
+)
+def update_bar_selector(value, clickData):
+    holder = []
+    if clickData:
+        holder.append(str(int(clickData["points"][0]["x"])))
+    if value:
+        for x in value["points"]:
+            holder.append(str(int(x["x"])))
+    return list(set(holder))
